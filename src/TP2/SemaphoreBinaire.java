c986bcf3c80e88d3c6a96acbcd7c09367cc7f548
Nathan Lavallart <nathan.lavallart@ens.uvsq.fr>
2020-02-28 12:10:39 +0100

    Version Prof

---
+++ src/TP2/SemaphoreBinaire.java
@@ -0,0 +1,17 @@
+package TP2;
+
+import TP2.Semaphore;
+
+
+public final class SemaphoreBinaire extends Semaphore {
+public SemaphoreBinaire(int valeurInitiale){
+	super((valeurInitiale != 0) ? 1:0);
+	//System.out.print(valeurInitiale);
+}
+public final synchronized void syncSignal(){
+	super.syncSignal();
+	//System.out.print(valeur);
+	if (valeur>1) valeur = 1;
+}
+}
+
